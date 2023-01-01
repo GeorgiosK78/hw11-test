590842224190ecb2d1ae6aa7cfda207b488fc600
GeorgiosK78 <117475305+GeorgiosK78@users.noreply.github.com>
2023-01-01 13:31:28 +0100

    Update Author.java

--- HW8/StaticAnalysisExample/src/_package_1/Author.java
+++ HW8/StaticAnalysisExample/src/_package_1/Author.java
@@ -1,7 +1,7 @@
 package _package_1;
 
 
-public class Author {
+public class Author { 
 
 	public Author(String lastPart, String lastPart2, String vonPart, String firstPart, String jrPart) {
 		// TODO Auto-generated constructor stub
